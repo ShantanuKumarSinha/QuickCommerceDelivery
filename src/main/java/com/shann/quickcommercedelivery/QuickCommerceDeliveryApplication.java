bcbfefdc3e69ddd1d49a728c89e739450ef1f375
Shantanu Kumar <shan.raj93@gmail.com>
2025-05-23 22:30:19 +0530

    (build:init) init build

---
+++ src/main/java/com/shann/quickcommercedelivery/QuickCommerceDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.shann.quickcommercedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCommerceDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickCommerceDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shann/quickcommercedelivery/QuickCommerceDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shann.quickcommercedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickCommerceDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
